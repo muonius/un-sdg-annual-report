96fb0294f32ef0a3139a07b1465bcfb24d754827
Yang <freda.y.zhao@gmail.com>
2023-04-04 16:14:07 -0400

    update

--- edgebinding.js
+++ edgebinding.js
@@ -1,10 +1,6 @@
-var div = d3.select("body").append("div")	
-    .attr("class", "tooltip")				
-    .style("opacity", 0);
-
-var diameter = 1152,
+var diameter = 960,
     radius = diameter / 2,
-    innerRadius = radius - 300;
+    innerRadius = radius - 120;
 
 var cluster = d3.cluster()
     .size([360, innerRadius]);
@@ -14,19 +10,16 @@ var line = d3.radialLine()
     .radius(function(d) { return d.y; })
     .angle(function(d) { return d.x / 180 * Math.PI; });
 
-var svg = d3.select("div#container")
-  .append("svg")
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "-576 -576 1152 1152")
-  .classed("svg-content", true);
-svg
-    .append("g")
+var svg = d3.select("body").append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+  .append("g")
     .attr("transform", "translate(" + radius + "," + radius + ")");
 
 var link = svg.append("g").selectAll(".link"),
     node = svg.append("g").selectAll(".node");
 
-d3.json("https://gist.githubusercontent.com/papillonbee/1ef1c7b696112ea1d08bb32de9a6a2c7/raw/23a85e5aee94c45f171e13d9e766d73a2cc6b1be/Book2.json", function(error, classes) {
+d3.json("flare.json", function(error, classes) {
   if (error) throw error;
 
   var root = packageHierarchy(classes)
@@ -34,13 +27,6 @@ d3.json("https://gist.githubusercontent.com/papillonbee/1ef1c7b696112ea1d08bb32d
 
   cluster(root);
 
-  var x = packageImports(root.leaves());
-  var arr = [], arr2 = [];
-  for(var i = 0; i < x.length; i++){
-    arr.push(x[i][0].data.key);
-    arr2.push(x[i][x[i].length - 1].data.key);
-  }
-  console.log(arr.length);
   link = link
     .data(packageImports(root.leaves()))
     .enter().append("path")
@@ -48,42 +34,9 @@ d3.json("https://gist.githubusercontent.com/papillonbee/1ef1c7b696112ea1d08bb32d
       .attr("class", "link")
       .attr("d", line);
 
-  /*node.data(root.leaves()).enter()
-    .append("a")
-    .attr("xlink:href", "http://en.wikipedia.org/wiki/")
-    .append("rect")
-    .attr("height", 100)
-    .attr("width", 200)
-    .style("fill", "lightgreen")*/
-    /*.attr("rx", function(d){ return d.x; })
-    .attr("ry", function(d){ return d.y; })*/;
-  //console.log(root.leaves()[98].data.url);
   node = node
     .data(root.leaves())
-  
-  /*node.enter()
-    .append("a")
-    .attr("class", "node")
-    .attr("dy", "0.31em")
-    .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
-    .attr("xlink:href", "http://en.wikipedia.org/wiki/")
-    .append("rect")
-    .attr("height", 10)
-    .attr("width", 20)
-    .style("fill", "lightgreen");*/
-  node
-    .enter().append("a")
-      .attr("class", "node")
-      .attr("dy", "0.31em")
-      .attr("xlink:href", function(d){
-    if(d.data.url === undefined){
-    }
-    else{
-      //console.log(d.data.url);
-      return d.data.url;
-    }   
-  })
-      .append("text")
+    .enter().append("text")
       .attr("class", "node")
       .attr("dy", "0.31em")
       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
@@ -106,15 +59,6 @@ function mouseovered(d) {
   node
       .classed("node--target", function(n) { return n.target; })
       .classed("node--source", function(n) { return n.source; });
-  
-  div
-      .transition()		
-      .duration(200)		
-      .style("opacity", .9);		
-  div
-      .html(d.data.key + "<br/>")	
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
 }
 
 function mouseouted(d) {
@@ -125,10 +69,6 @@ function mouseouted(d) {
   node
       .classed("node--target", false)
       .classed("node--source", false);
-  div
-      .transition()		
-      .duration(500)		
-      .style("opacity", 0);	
 }
 
 // Lazily construct the package hierarchy from class names.
@@ -174,78 +114,3 @@ function packageImports(nodes) {
 
   return imports;
 }
-
-function update(year){
-  d3.json("https://gist.githubusercontent.com/papillonbee/1ef1c7b696112ea1d08bb32de9a6a2c7/raw/23a85e5aee94c45f171e13d9e766d73a2cc6b1be/Book2.json", function(error, classes) {
-    if (error) throw error;
-    classes = classes.filter(function(d, i){
-      if(parseInt(d.name.substr(0, d.name.lastIndexOf("."))) === year){
-        return d;
-      }
-      if(d.name.substr(0, d.name.lastIndexOf(".")) === 'x'){
-        return d;
-      }
-      if(d.name.substr(0, d.name.lastIndexOf(".")) === 'y'){
-        return d;
-      }
-      if(d.name.substr(0, d.name.lastIndexOf(".")) === 'z'){
-        return d;
-      }
-    })
-    
-    console.log(classes);
-    var root = packageHierarchy(classes)
-      .sum(function(d) { return d.size; });
-  
-    cluster(root);
-    
-    
-    link = link.data(packageImports(root.leaves()));
-    link.exit().remove();
-    link
-      .enter()
-      .append("path")
-      .merge(link)
-      .each(function(d) { d.source = d[0], d.target = d[d.length - 1]; })
-      .attr("class", "link")
-      .attr("d", line);
-    
-    
-    node = node.data(root.leaves());
-    node.exit().remove();
-    node
-      .enter()
-      .append("text")
-      .merge(node)
-      .attr("class", "node")
-      .attr("dy", "0.31em")
-      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
-      .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-      .text(function(d) { return d.data.key; })
-      .on("mouseover", mouseovered)
-      .on("mouseout", mouseouted);
-  });
-}
-/*var svg2 = d3.select("body").append("svg").attr("class", "svg2").attr("width", 449).attr("height", 249);
-
-svg2.append("a")
-    .attr("xlink:href", "http://en.wikipedia.org/wiki/")
-    .append("rect")  
-    .attr("x", 100)
-    .attr("y", 50)
-    .attr("height", 100)
-    .attr("width", 200)
-    .style("fill", "lightgreen")
-    .attr("rx", 10)
-    .attr("ry", 10);
-
-svg2.append("text")
-    .attr("x", 200)
-    .attr("y", 100)
-    .style("fill", "black")
-    .style("font-size", "20px")
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .style("pointer-events", "none")
-    .text("Click me");
-//update(2558);*/
