c1ebec22be31f337f75b9fcc57334ae907828d57
Yang <freda.y.zhao@gmail.com>
2023-04-05 02:38:27 -0400

    visual 1 & 2

---
+++ v2_vertical_sankey_backup/sankey2.js
@@ -0,0 +1,435 @@
+var json = {
+    "nodes": [
+      {
+        "name": "Development Emergency Modality"
+      },
+      {
+        "name": "Climate action & energy transformation"
+      },
+      {
+        "name": "Decent jobs & universal social protection"
+      },
+      {
+        "name": "Digital transformation"
+      },
+      {
+        "name": "Food systems transformation"
+      },
+      {
+        "name": "SDG localization"
+      },
+      {
+        "name": "Solutions for internal displacements"
+      },
+      {
+        "name": "Transforming education"
+      },
+      {
+        "name": "$7,229,741"
+      },
+      {
+        "name": "$10,185,912"
+      },
+      {
+        "name": "$1,995,507"
+      },
+      {
+        "name": "$18,625,603"
+      },
+      {
+        "name": "$3,890,591"
+      },
+      {
+        "name": "$999,890"
+      },
+      {
+        "name": "$900,000"
+      },
+      {
+        "name": "SDG catalytic investments"
+      },
+      {
+        "name": "$51,552,114"
+      },
+      {
+        "name": "$7,100,000"
+      },
+      {
+        "name": "$10,193,055"
+      },
+      {
+        "name": "SIDS resilience"
+      },
+      {
+        "name": "$5,085,791"
+      },
+      {
+        "name": "$20,747,549"
+      },
+      {
+        "name": "$16,855,165"
+      },
+      {
+        "name": "$9,221,800"
+      },
+      {
+        "name": "$9,419,150"
+      },
+      {
+        "name": "$714,400"
+      },
+      {
+        "name": "$2,168,600"
+      },
+      {
+        "name": "Integrated social protection"
+      },
+      {
+        "name": "$9,221,800"
+      },
+      {
+        "name": "$9,419,150"
+      },
+      {
+        "name": "$714,400"
+      },
+      {
+        "name": "SDG financing enabling envrionment"
+      },
+      {
+        "name": "$2,168,600"
+      },
+      {
+        "name": "$9,221,800"
+      },
+      {
+        "name": "$9,419,150"
+      },
+      {
+        "name": "$714,400"
+      }
+    ],
+    "links": [
+      {
+        "source": 0,
+        "target": 1,
+        "value": 1,
+      },
+      {
+        "source": 0,
+        "target": 2,
+        "value": 1
+      },
+      {
+        "source": 0,
+        "target": 3,
+        "value": 1
+      },
+      {
+        "source": 0,
+        "target": 4,
+        "value": 1
+      },
+      {
+        "source": 0,
+        "target": 5,
+        "value": 1
+      },
+      {
+        "source": 0,
+        "target": 6,
+        "value": 1
+      },
+      {
+        "source": 0,
+        "target": 7,
+        "value": 1
+      },
+
+      {
+        "source": 1,
+        "target": 8,
+        "value": 1,
+      },
+      {
+        "source": 2,
+        "target": 9,
+        "value": 1
+      },
+      {
+        "source": 3,
+        "target": 10,
+        "value": 1
+      },
+      {
+        "source": 4,
+        "target": 11,
+        "value": 1
+      },
+      {
+        "source": 5,
+        "target":12,
+        "value": 1
+      },
+      {
+        "source": 6,
+        "target": 13,
+        "value": 1
+      },
+      {
+        "source": 7,
+        "target": 14,
+        "value": 1
+      },
+
+      {
+        "source": 15,
+        "target":1,
+        "value": 1
+      },
+      {
+        "source": 15,
+        "target": 2,
+        "value": 1
+      },
+      {
+        "source": 15,
+        "target":4,
+        "value": 1
+      },
+
+      {
+        "source": 1,
+        "target":16,
+        "value": 1
+      },
+      {
+        "source": 2,
+        "target": 17,
+        "value": 1
+      },
+      {
+        "source": 4,
+        "target":18,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 1,
+        "value": 1,
+      },
+      {
+        "source": 19,
+        "target": 2,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 3,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 4,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 5,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 6,
+        "value": 1
+      },
+      {
+        "source": 19,
+        "target": 7,
+        "value": 1
+      },
+      {
+        "source": 1,
+        "target": 20,
+        "value": 1,
+      },
+      {
+        "source": 2,
+        "target": 21,
+        "value": 1
+      },
+      {
+        "source": 3,
+        "target": 22,
+        "value": 1
+      },
+      {
+        "source": 4,
+        "target": 23,
+        "value": 1
+      },
+      {
+        "source": 5,
+        "target":24,
+        "value": 1
+      },
+      {
+        "source": 6,
+        "target": 25,
+        "value": 1
+      },
+      {
+        "source": 7,
+        "target": 26,
+        "value": 1
+      },
+      {
+        "source": 27,
+        "target": 2,
+        "value": 1
+      },
+      {
+        "source": 27,
+        "target": 3,
+        "value": 1
+      },
+      {
+        "source": 27,
+        "target": 4,
+        "value": 1
+      },
+      {
+        "source": 2,
+        "target": 28,
+        "value": 1
+      },
+      {
+        "source": 3,
+        "target": 29,
+        "value": 1
+      },
+      {
+        "source": 4,
+        "target": 30,
+        "value": 1
+      },
+      {
+        "source": 31,
+        "target": 5,
+        "value": 1
+      },
+      {
+        "source": 31,
+        "target": 6,
+        "value": 1
+      },
+      {
+        "source": 31,
+        "target": 7,
+        "value": 1
+      },
+      {
+        "source": 31,
+        "target": 1,
+        "value": 1
+      },
+      {
+        "source": 5,
+        "target": 32,
+        "value": 1
+      },
+      {
+        "source": 6,
+        "target": 33,
+        "value": 1
+      },
+      {
+        "source": 7,
+        "target": 34,
+        "value": 1
+      },
+      {
+        "source": 1,
+        "target": 35,
+        "value": 1
+      }
+    ]
+  };
+  
+  var margin = {top: 20, right: 1, bottom: 6, left: 20},
+      width = 960 - margin.left - margin.right,
+      height = 660 - margin.top - margin.bottom;
+  
+  var formatNumber = d3.format(",.0f"),
+      format = function(d) { return formatNumber(d) + " TWh"; },
+      color = d3.scale.category20();
+  
+  var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //change to vertical  
+
+      // svg.attr("transform", `rotate(-90, 0, 0) translate(-${width}, 0)`);
+  
+  var sankey = d3.sankey()
+      .nodeWidth(20)
+      .nodePadding(10)
+      .size([width, height]);
+  
+  var path = sankey.link();
+  
+  
+  sankey
+        .nodes(json.nodes)
+        .links(json.links)
+        .layout(4);
+  
+    var link = svg.append("g").selectAll(".link")
+        .data(json.links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+        .sort(function(a, b) { return b.dy - a.dy; });
+  
+    link.append("title")
+        .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+  
+    var node = svg.append("g").selectAll(".node")
+        .data(json.nodes)
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .call(d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("dragstart", function() { this.parentNode.appendChild(this); })
+        .on("drag", dragmove));
+  
+    node.append("rect")
+        .attr("height", function(d) { return d.dy; })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+        .style("stroke", function(d) { return d3.rgb(d.color).darker(0); })
+      .append("title")
+        .text(function(d) { return d.name + "\n" + format(d.value); });
+  
+    node.append("text")
+        .attr("x", -6)
+        .attr("y", function(d) { return d.dy / 2; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .attr("transform", null)
+        .text(function(d) { return d.name; })
+      .filter(function(d) { return d.x < width / 2; })
+        .attr("x", 30 + sankey.nodeWidth())
+        .attr("text-anchor", "start");
+  
+    function dragmove(d) {
+      d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+      sankey.relayout();
+      link.attr("d", path);
+    }
+  
